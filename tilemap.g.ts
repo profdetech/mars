b38211506c1e171864f714688341f6ca63ee1634
profdetech <emmanuel.mouronval@gmail.com>
2020-11-11 22:00:49 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,7 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+}
+// Code généré automatiquement. Ne pas modifier.
